45af81ac9eeeffb55bf027483c7a2507a39051cc
william borges <williamborgesaldrighii@gmail.com>
2024-08-21 15:17:11 -0300

    dsds

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,34 +4,43 @@ import type { AppProps } from 'next/app'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import { useEffect } from 'react'
-import disableDevtool from 'disable-devtool'
-
-// Configuração para o disable-devtool
-const config = {
-  interval: 500,
-  disableMenu: true,
-  disableSelect: true,
-  disableCopy: true,
-  disableCut: true,
-  disablePaste: false,
-  clearLog: true,
-  clearIntervalWhenDevOpenTrigger: true,
-  detectors: [
-    0, // RegToString
-    4, // FuncToString
-    6, // DebugLib
-  ],
-  ondevtoolopen: (type: number, next: () => void) => {
-    alert('Atenção: Ferramentas de desenvolvedor detectadas!')
-    next()
-  },
-  rewriteHTML: '<html><body><h1>Conteúdo Protegido</h1></body></html>',
-  disableIframeParents: true,
-}
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    disableDevtool(config)
+    // Verifica se o código está sendo executado no navegador
+    if (typeof window !== 'undefined') {
+      import('disable-devtool')
+        .then(({ default: disableDevtool }) => {
+          // Configuração para o disable-devtool
+          const config = {
+            interval: 500,
+            disableMenu: true,
+            disableSelect: true,
+            disableCopy: true,
+            disableCut: true,
+            disablePaste: false,
+            clearLog: true,
+            clearIntervalWhenDevOpenTrigger: true,
+            detectors: [
+              0, // RegToString
+              4, // FuncToString
+              6, // DebugLib
+            ],
+            ondevtoolopen: (type: number, next: () => void) => {
+              alert('Atenção: Ferramentas de desenvolvedor detectadas!')
+              next()
+            },
+            rewriteHTML:
+              '<html><body><h1>Conteúdo Protegido</h1></body></html>',
+            disableIframeParents: true,
+          }
+
+          disableDevtool(config)
+        })
+        .catch((error) => {
+          console.error('Erro ao carregar disable-devtool:', error)
+        })
+    }
   }, [])
 
   return <Component {...pageProps} />
