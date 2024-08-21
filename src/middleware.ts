68b2575825954bc6bfdb866d71758217cf4bff99
william borges <williamborgesaldrighii@gmail.com>
2024-08-21 20:52:51 -0300

    dsdsds

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,18 +4,15 @@ export function middleware(req: NextRequest) {
   const userAgent = req.headers.get('user-agent')
   const referer = req.headers.get('referer')
 
-  // Verificação de User-Agent suspeito
-  if (
-    userAgent &&
-    /saveweb|saveweb2zip|wget|curl|httpclient|webzip/i.test(userAgent)
-  ) {
+  // Ajuste na verificação do User-Agent para ser menos agressiva
+  if (userAgent && /saveweb2zip|wget|curl|httpclient|webzip/i.test(userAgent)) {
     return new Response('<html><body></body></html>', {
       status: 200,
       headers: { 'Content-Type': 'text/html' },
     })
   }
 
-  // Verificação de Referer
+  // Verificação de Referer inválido
   if (referer && !referer.startsWith('https://seusite.com')) {
     return new Response('<html><body></body></html>', {
       status: 200,
@@ -26,13 +23,10 @@ export function middleware(req: NextRequest) {
   // Verificação de requisições rápidas sucessivas
   const timestamp = Date.now()
 
-  // Obtenha o valor do cookie e converta-o para string
   const lastRequestTime = req.cookies.get('lastRequestTime')?.value || '0'
 
-  // Armazene o novo timestamp como string
   req.cookies.set('lastRequestTime', timestamp.toString())
 
-  // Compare o tempo entre requisições
   if (timestamp - parseInt(lastRequestTime) < 1000) {
     return new Response('<html><body></body></html>', {
       status: 200,
